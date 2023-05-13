5945409bb54f895fde1e60e1ec4fcdcb679e71f2
Que-sar <arato.viktor@gmail.com>
2023-05-13 23:51:30 +0100

    Basic changes into making it mvvm for contact us feature

--- app/src/main/java/com/assessment/phishsafe/ContactUsModel.kt
+++ app/src/main/java/com/assessment/phishsafe/ContactUsModel.kt
@@ -1,5 +1,7 @@
 package com.assessment.phishsafe
 
+import android.content.Context
+import android.widget.Toast
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 
@@ -7,9 +9,14 @@ class ContactUsModel {
 
     private lateinit var database : DatabaseReference
 
-    fun uploadContactDetails(id: String, phoneNumber: String, email:String, text: String)
+    fun uploadContactDetails(context: Context, id: String, contactData: ContactData)
     {
-        database = FirebaseDatabase.getInstance("https://phishsafe-9253b-default-rtdb.europe-west1.firebasedatabase.app").getReference("ContactDetails")
 
+        database = FirebaseDatabase.getInstance("https://phishsafe-9253b-default-rtdb.europe-west1.firebasedatabase.app").getReference("ContactDetails")
+        database.child(id).setValue(contactData).addOnSuccessListener{
+                Toast.makeText(context, "We have received your message!", Toast.LENGTH_LONG).show()
+                }.addOnFailureListener{
+                    Toast.makeText(context, "Please check your network connection and try again.", Toast.LENGTH_LONG).show()
+        }
     }
 }
--- app/src/main/java/com/assessment/phishsafe/ContactUsViewModel.kt
+++ app/src/main/java/com/assessment/phishsafe/ContactUsViewModel.kt
@@ -1,4 +1,20 @@
 package com.assessment.phishsafe
 
 class ContactUsViewModel {
+
+    // Viewmodel stuff
+    // Unique id using val uniqueId = UUID.randomUUID().toString()
+    // 200 character handle for text field
+    // calling functions for model
+    // saving it if not sent using sharedpref
+    // put all into a package named ContactUsFeature
+    // only accept numbers in phone number
+    // basic checks for email
+    // check if all required is filled out
+
+    data class ContactData(
+        val phoneNumber: String,
+        val email: String,
+        val text: String
+    )
 }
